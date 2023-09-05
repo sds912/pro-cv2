fc9a8369c82d57fdab1294c14182b605b4fc37cb
EXT_SENGHOR0707 <papababacarngor.senghor@ornage-sonatel.com>
2023-09-05 11:52:01 +0000

    init project

---
+++ procv/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBejmmStWCr49Oq5uVHWK3bDUjZzSuhHrg",
+    authDomain: "pro-cv-3c0d2.firebaseapp.com",
+    projectId: "pro-cv-3c0d2",
+    storageBucket: "pro-cv-3c0d2.appspot.com",
+    messagingSenderId: "1013016657936",
+    appId: "1:1013016657936:web:30a9aae9d8b66584604a95",
+    measurementId: "G-19XT9JZ7LR"
+  };
